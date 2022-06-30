6589e26be474569237b69fbbc5dc0d6bd836485f
marcelo <marcelofavaroborges@gmail.com>
2022-06-30 10:01:31 -0300

    feat: add prettier, eslint and configurations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
   return (
@@ -14,7 +13,7 @@ const Home: NextPage = () => {
 
       <h1> Teste </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
